92220f046b3514fbbac787f9f205427e32076620
pedrofixe <yurbypedro@hotmail.com>
2017-10-17 17:25:34 +0100

    Started on praias.h

---
+++ praias.h
@@ -0,0 +1,16 @@
+#ifndef PRAIAS_INCLUDED
+#define PRAIAS_INCLUDED
+
+
+class praiaFluvial
+{
+	//Concelho
+	//coordenadas
+	bool bandeiraAzul;
+	unsigned int capacidade;
+	//pontos turisticos
+
+}
+
+
+#endif
