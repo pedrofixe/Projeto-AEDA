5b6dfeba7ccbca1f7a0cd7c95b979602b3774680
pedrofixe <yurbypedro@hotmail.com>
2017-11-28 16:15:36 +0000

    Added test.cpp. Changed .gitignore to accept test.cpp

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "menu.h"
+#include "praias.h"
+#include "algoritmos.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "\n";
+
+	menu main;
+	main.Begin();
+
+	cout << "\n\n";
+	return 0;
+}
